0271369d07db3fe3678be527028f171066d1f915
Bittu Mondal <mondalbittu169@gmail.com>
2024-11-09 20:16:20 +0530

    Add files via upload

---
+++ type_text.js
@@ -0,0 +1,13 @@
+
+'use strict';
+
+
+let typed = new Typed('#typed-text', {
+    strings: ["Web Developer", "Content Creator","Travel Freak","Gamer","Programmer"],
+    typeSpeed: 50,
+    loop: true,
+    backDelay: 5000,
+    showCursor: false,
+    backSpeed: 50
+  });
+  
