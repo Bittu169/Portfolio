1ad4937cab50ef0b9535fc7a8530ce7678fa7db5
Bittu Mondal <mondalbittu169@gmail.com>
2024-11-09 22:55:45 +0530

    Update slide.js

--- slide.js
+++ slide.js
@@ -15,7 +15,7 @@ const sliderFun = function() {
         {
             skillName: 'Python',
             skillInfo: 'Python is an interpreted high-level general-purpose programming language. Its design philosophy emphasizes code readability with its use of significant indentation. Its language constructs as well as its object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects',
-            skillImagePath: './img/python-symbol.png'
+            skillImagePath: './img/python.png'
         },
         {
             skillName: 'Java',
